0ce63ead6832f59d072aa6b445940953b05ded66
Remigiusz Zudzin <remzuothers@gmail.com>
2020-07-13 11:11:13 +0200

    Learning functions

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-// show the title
-if (+(1.1 + 1.3).toFixed(2) === 2.4) {
-    showMessage('true')
-} else {
-    showMessage('false');
+let myFunction = function loggingFunction() {
+    console.log('Here is a message')
+};
+
+myFunction();
+
+function showMessage(message) {
+    console.log(message);
 }
 
+showMessage('jeden', 'dwa');
+
+let myFunction2 = function (message, firstName) {
+    console.log(message);
+    console.log(firstName);
+}
+
+myFunction2('Hello', 'Remik');
+
+let key = 42;
+
+function getSecretCode(value) {
+
+    let keyGenerator = function () {
+        let key = 12;
+        console.log('in keyGenerator: ', key);
+    }
+
+    let code = value * keyGenerator();
+    console.log('in getSecretCode: ', key);
+    return code;
+}
+
+changePercentOff(30);
+
+showMessage(getSecretCode(3));
 
 /*
  Anything...  
--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
 ﻿function showMessage(message) {
     document.getElementById('message').textContent = message;
 }
+
+function changePercentOff(percentage) {
+    document.getElementById('percent-off').textContent = percentage + "% OFF";
+}
